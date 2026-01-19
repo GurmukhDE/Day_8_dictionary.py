4dc6205701307a08a47745863fe1ec92319c2963
GurmukhDE <gurmukhsinghgps@gmail.com>
2026-01-19 23:57:45 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+l= [12,3,35,45,456,4,54,4] #List is the hetrogenous data type(anytype of data list can contain)
+
+
+# In[2]:
+
+
+type(l)
+
+
+# In[9]:
+
+
+#Hetrogenous means
+
+l1 = ["singh","gur",True, 4+5j, 34,45,46,["dfgdr",46456,67]] #Nested list(list inside list)
+
+
+# In[10]:
+
+
+l1
+
+
+# In[11]:
+
+
+type(l1)
+
+
+# In[14]:
+
+
+l1[3:4]
+
+
+# In[25]:
+
+
+l1[0:3:2]
+
+
+# In[17]:
+
+
+l1[4]
+
+
+# In[18]:
+
+
+l1[3]
+
+
+# In[28]:
+
+
+'spam'
+
+
+# In[35]:
+
+
+a = "my_string_1"
+
+
+# In[36]:
+
+
+a = "1st_string"
+
+
+# In[37]:
+
+
+a = "foo"
+
+
+# In[34]:
+
+
+a =_
+
+
+# In[40]:
+
+
+"my_string_1"
+
+
+# In[42]:
+
+
+"1st_string"
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
